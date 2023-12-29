51a1953480a45bc3dc346841d62e3cfbf4287c79
Andrew Eggleston <egglestonandrew927@gmail.com>
2023-12-29 00:36:18 -0500

    :x: i do not like natural scrolling :x:

--- home-manager/andreweggleston/features/desktop/sway/natural-scrolling.nix
+++ home-manager/andreweggleston/features/desktop/sway/natural-scrolling.nix
@@ -2,15 +2,15 @@
 {
   wayland.windowManager.sway.config.input = {
     "type:pointer" = { 
-      natural_scroll = "enabled";
+      natural_scroll = "disabled";
     };
 
     "type:touchpad" = { 
-      natural_scroll = "enabled";
+      natural_scroll = "disabled";
     };
 
     "type:mouse" = {
-      natural_scroll = "enabled";
+      natural_scroll = "disabled";
     };
   };
 }
