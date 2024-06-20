0c39c8adfcc52e1dc37c02f341f79235af2164e0
Andrew Eggleston <egglestonandrew927@gmail.com>
2024-06-19 23:32:27 -0400

    ssh: added blue darwin m2 as authorized key

--- nixos/users.nix
+++ nixos/users.nix
@@ -6,6 +6,7 @@
         shell = pkgs.fish;
 
         openssh.authorizedKeys.keys = [ 
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDL2iuw70Vw9fghAGz6sqsvLMSVpFa8xxy7iIiY99nWZ05NjRQYow8GUNI3zaTBxc+H+sqO1sQBv2nFarXSS5WTDzRlO6Zrd5RrWs9mNCmObillU35eXIhlFNMcuhaZd0YDH2N6KlD9uJJ3jIgmqlRXL+dUtsl/da4eqxWHbiVfaaWAvJ4esPru+2YVIbhrL2pt41HT1mfbWJWwmsD5ew14rjajh7FsHu3l4s87Fa4e2m7BieHSdDVqndvF1tMx4eiB9Ph6qOAUMPM6ZcsMkOKdczxdj9O1PinSjQKO7Vz3ATUi1McRPjWei3Rz3Sru0oiDsZJbfkE8ZFI+0h72S2C8nk8R9r8D8RfYGstoaodOR0mSj9M3/JU7yBEYNgN3QnWqaw3tMOyYCKQqKBdj69wNdE3zimdnC1VrQybOeMU9hCYvY5ZzlnbwIfSV3X0VKm1696MGBt+7cSIgnxWS1kIafo0kn7nM03AK7XV71KG6ZGFyf5X9A4kDA5wehZAOCvk= andreweggleston"
           
         ];
     };
