7aee233a08c59fde0a77a3deb9bfaf128af435bf
Andrew Eggleston <egglestonandrew927@gmail.com>
2024-07-03 15:45:56 -0400

    color: woodland

--- home-manager/andreweggleston/global/colors.nix
+++ home-manager/andreweggleston/global/colors.nix
@@ -9,7 +9,7 @@
     inputs.nix-colors.homeManagerModule
   ];
 
-  colorScheme = lib.mkDefault inputs.nix-colors.colorSchemes.nord;
+  colorScheme = lib.mkDefault inputs.nix-colors.colorSchemes.woodland;
 
   # write the color scheme to a CSS file for future reference
   home.file.".config/colors/colorscheme.css".text = let
