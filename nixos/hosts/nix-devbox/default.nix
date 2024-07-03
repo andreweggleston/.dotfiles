03632120ffb164185d2ce0d2578c74a1c537a679
Andrew Eggleston <egglestonandrew927@gmail.com>
2024-07-03 15:47:30 -0400

    nix-devbox: enable openssh x forwarding

--- nixos/hosts/nix-devbox/default.nix
+++ nixos/hosts/nix-devbox/default.nix
@@ -1,55 +1,44 @@
-# Host config for 14" M1-Pro macbook pro 
-
-{ config, pkgs, lib, inputs, outputs, ... }:
+# Host config for 14" M1-Pro macbook pro
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+  config,
+  pkgs,
+  lib,
+  inputs,
+  outputs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
 
-      
-      ../../common.nix
+    ../../common.nix
 
-      # enable various features
-      ../../features/sound.nix
+    # enable various features
+    ../../features/sound.nix
 
-      # key mappings
-      # outputs.nixosModules.dual-function-keys
-      # ../../features/key-mapping/caps-to-ctrl-esc.nix
-      # ../../features/key-mapping/right-alt-to-ctrl-b.nix
+    # key mappings
+    # outputs.nixosModules.dual-function-keys
+    # ../../features/key-mapping/caps-to-ctrl-esc.nix
+    # ../../features/key-mapping/right-alt-to-ctrl-b.nix
 
-      # loopback video (for virtual webcam)
-      # outputs.nixosModules.v4l2-loopback
-    ];
+    # loopback video (for virtual webcam)
+    # outputs.nixosModules.v4l2-loopback
+  ];
 
   home-manager.users.andreweggleston = import ../../../home-manager/andreweggleston/hosts/nix-devbox.nix;
 
-  # Webcam
-  # v4l2-loopback = {
-  #   enable = true;
-  #   devices = [
-  #     {
-  #       number = 0;
-  #       label = "Droidcam";
-  #     }
-  #   ];
-  # };
-
-
-  # services.speakersafetyd.enable = true;
-
-  environment.systemPackages = [ 
-
+  environment.systemPackages = [
   ];
-
 
   networking = {
     hostName = "nix-devbox";
-	nameservers = ["192.168.2.2"];
+    nameservers = ["192.168.2.2"];
   };
+
+  services.openssh.settings.X11Forwarding = true;
 
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
 
   system.stateVersion = "23.11";
 }
-
