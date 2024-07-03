fc2be479b5eb94f014a164678780805cc85a07cd
andreweggleston <egglestonandrew927@gmail.com>
2024-07-03 12:04:09 -0400

    colors: colorScheme.colors -> colorScheme.palette; colorScheme.kind -> colorScheme.variant

--- home-manager/andreweggleston/features/desktop/sway/sway.nix
+++ home-manager/andreweggleston/features/desktop/sway/sway.nix
@@ -1,21 +1,22 @@
-{ lib
-, config
-, pkgs
-, inputs
-, ... }:
-let
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}: let
   inherit (lib) mkIf;
   inherit (lib.attrsets) optionalAttrs;
   inherit (lib.lists) optionals;
   inherit (lib.strings) optionalString;
 
-  output-config = (import ./monitors.nix {
+  output-config = import ./monitors.nix {
     inherit lib;
     inherit (config) monitors;
-  });
+  };
 
   cursor-size = 24;
-  
+
   background-image = config.wallpaper;
   lock-cmd = "${pkgs.swaylock-effects}/bin/swaylock -S --daemonize";
 
@@ -23,7 +24,7 @@ let
     exec ${pkgs.dbus}/bin/dbus-run-session sway $SWAY_CLI_FLAGS
   '';
 
-  color-config = with config.colorScheme.colors; ''
+  color-config = with config.colorScheme.palette; ''
     # class                 border     backgr.    text       indicator    child_border
     client.focused          #${base04} #${base0D} #${base02} #${base0E}   #${base0D}
     client.focused_inactive #${base02} #${base02} #${base06} #${base0E}   #${base0E}
@@ -33,9 +34,7 @@ let
 
     client.background       #${base00}
   '';
-
 in {
-
   imports = [
     ./waybar
     ./swaylock.nix
@@ -45,9 +44,9 @@ in {
   programs.fish.loginShellInit = ''
     # if running from tty1, start sway
     set TTY1 (tty)
-    
+
     if [ "$TTY1" = "/dev/tty1" ]
-      exec ${start-sway}/bin/start-sway 
+      exec ${start-sway}/bin/start-sway
     end
   '';
 
@@ -62,62 +61,64 @@ in {
   };
 
   wayland.windowManager.sway = {
-      enable = true;
-      wrapperFeatures.gtk = true;
+    enable = true;
+    wrapperFeatures.gtk = true;
 
-      config = { 
-        modifier = "Mod4";
-        terminal = config.home.sessionVariables.TERMINAL;
-        output = output-config // {
+    config = {
+      modifier = "Mod4";
+      terminal = config.home.sessionVariables.TERMINAL;
+      output =
+        output-config
+        // {
           "*" = {
             bg = "${background-image} fill";
           };
         };
-        window.hideEdgeBorders = "both";
-        fonts = {
-          names = [ "FiraCode Nerd Font" ];
-          style = "Regular";
-          size = 12.0;
-        };
-
-        keybindings = 
-          let
-            modifier = config.wayland.windowManager.sway.config.modifier;
-          in lib.mkOptionDefault {
-            "${modifier}+space" = "exec ${pkgs.albert}/bin/albert show";
-            "${modifier}+l" = "exec ${lock-cmd}";
-            "${modifier}+i" = "exec firefox";
-            "${modifier}+Shift+i" = "exec firefox --private-window";
-            "${modifier}+Shift+k" = "kill";
-            "${modifier}+t" = "layout tabbed";
-          };
-
-        focus.wrapping = "no";
-
-        startup = [
-          { command = "eval $(gnome-keyring-daemon --start --components=secrets);"; }
-          { command = "${pkgs.albert}/bin/albert"; }
-        ];
-
-        # set cursor size
-        seat."*".xcursor_theme = "Vanilla-DMZ ${builtins.toString cursor-size}";
+      window.hideEdgeBorders = "both";
+      fonts = {
+        names = ["FiraCode Nerd Font"];
+        style = "Regular";
+        size = 12.0;
       };
 
-      systemd.enable = true;
+      keybindings = let
+        modifier = config.wayland.windowManager.sway.config.modifier;
+      in
+        lib.mkOptionDefault {
+          "${modifier}+space" = "exec ${pkgs.albert}/bin/albert show";
+          "${modifier}+l" = "exec ${lock-cmd}";
+          "${modifier}+i" = "exec firefox";
+          "${modifier}+Shift+i" = "exec firefox --private-window";
+          "${modifier}+Shift+k" = "kill";
+          "${modifier}+t" = "layout tabbed";
+        };
 
-      extraConfig = color-config;
+      focus.wrapping = "no";
 
-      extraSessionCommands = ''
-        export QT_AUTO_SCREN_SCALING_FACTOR=1 
-        export QT_QPA_PLATFORM=wayland
-        export QT_WAYLAND_DISABLE_WINDOW_DECORATIONS=1
-        export GDK_BACKEND=wayland
-        export MOZ_ENABLE_WAYLAND=1
-        export XDG_SESSION_TYPE=wayland
-        export XDG_SESSION_DESKTOP=sway
-        export XDG_CURRENT_DESKTOP=sway
-        export _JAVA_AWT_WM_NONREPARENTING=1
-      '';
+      startup = [
+        {command = "eval $(gnome-keyring-daemon --start --components=secrets);";}
+        {command = "${pkgs.albert}/bin/albert";}
+      ];
+
+      # set cursor size
+      seat."*".xcursor_theme = "Vanilla-DMZ ${builtins.toString cursor-size}";
+    };
+
+    systemd.enable = true;
+
+    extraConfig = color-config;
+
+    extraSessionCommands = ''
+      export QT_AUTO_SCREN_SCALING_FACTOR=1
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOW_DECORATIONS=1
+      export GDK_BACKEND=wayland
+      export MOZ_ENABLE_WAYLAND=1
+      export XDG_SESSION_TYPE=wayland
+      export XDG_SESSION_DESKTOP=sway
+      export XDG_CURRENT_DESKTOP=sway
+      export _JAVA_AWT_WM_NONREPARENTING=1
+    '';
   };
 
   # cursor config
