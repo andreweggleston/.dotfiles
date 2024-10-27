50c7e9275af6215d9207ff7b3f7a82248eb7c305
Andrew Eggleston <egglestonandrew927@gmail.com>
2024-10-27 16:56:16 -0400

    drew-pc: use latest nvidia driver

--- nixos/hosts/drew-pc/default.nix
+++ nixos/hosts/drew-pc/default.nix
@@ -4,6 +4,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
@@ -24,6 +25,8 @@
 
   services.xserver.videoDrivers = ["nvidia"];
 
+  services.flatpak.enable = true;
+
   hardware = {
     nvidia = {
       modesetting.enable = true;
@@ -31,7 +34,14 @@
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "560.35.03";
+        sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+        sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
+        openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
+        settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
+        persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
+      };
     };
     opengl = {
       enable = true;
@@ -47,7 +57,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = pkgs.linuxPackages_6_6;
+    kernelPackages = pkgs.linuxPackages-rt_latest;
   };
   networking.hostName = "drew-pc"; # Define your hostname.
 
@@ -83,10 +93,14 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
+  environment.systemPackages = let
+    nvidiaEnabled = lib.elem "nvidia" config.services.xserver.videoDrivers;
+  in
+    lib.optionals nvidiaEnabled [
+      (config.hardware.nvidia.package.settings.overrideAttrs (oldAttrs: {
+        buildInputs = oldAttrs.buildInputs ++ [pkgs.vulkan-headers];
+      }))
+    ];
 
   system.stateVersion = "24.05"; # Did you read the comment?
 }
