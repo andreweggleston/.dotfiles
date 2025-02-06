b18689ca7ad5dff075d991477cb35e509899e1c6
Andrew Eggleston <egglestonandrew927@gmail.com>
2025-02-06 23:04:48 +0000

    router: fixed nftables logging bugs

--- nixos/hosts/router/routing.nix
+++ nixos/hosts/router/routing.nix
@@ -17,39 +17,39 @@
             type filter hook ingress devices = { ${interfaces.wan.name}, ${interfaces.lan.name} } priority -500;
 
             # block weird packets
-            tcp flags & (fin|syn) == (fin|syn) drop log prefix "NFT_DROP_WEIRD_FLAGS: " group 1
-            tcp flags & (syn|rst) == (syn|rst) drop log prefix "NFT_DROP_WEIRD_FLAGS: " group 1
-            tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|syn|rst|psh|ack|urg) drop log prefix "NFT_DROP_WEIRD_FLAGS: " group 1
-            tcp flags & (fin|syn|rst|psh|ack|urg) == 0 drop log prefix "NFT_DROP_WEIRD_FLAGS: " group 1
-            tcp flags syn tcp option maxseg size 0-500 drop log prefix "NFT_DROP_WEIRD_MSS: " group 1
+            tcp flags & (fin|syn) == (fin|syn) log prefix "NFT_DROP_WEIRD_FLAGS: " group 1 drop 
+            tcp flags & (syn|rst) == (syn|rst) log prefix "NFT_DROP_WEIRD_FLAGS: " group 1 drop 
+            tcp flags & (fin|syn|rst|psh|ack|urg) == (fin|syn|rst|psh|ack|urg) log prefix "NFT_DROP_WEIRD_FLAGS: " group 1 drop 
+            tcp flags & (fin|syn|rst|psh|ack|urg) == 0 log prefix "NFT_DROP_WEIRD_FLAGS: " group 1 drop 
+            tcp flags syn tcp option maxseg size 0-500 log prefix "NFT_DROP_WEIRD_MSS: " group 1 drop 
 
             # drop incoming packets that are pretending to be the internal address of the router (the lan interface)
-            ip saddr ${addresses.lan.ipv4.addr} drop log prefix "NFT_SPOOF_LAN: " group 2
-            ip6 saddr ${addresses.lan.ipv6.addr} drop log prefix "NFT_SPOOF_LAN6: " group 2
+            ip saddr ${addresses.lan.ipv4.addr} log prefix "NFT_SPOOF_LAN: " group 2 drop 
+            ip6 saddr ${addresses.lan.ipv6.addr} log prefix "NFT_SPOOF_LAN6: " group 2 drop 
 
             # drop incoming packets on WAN that are pretending to be internal ULA traffic
-            ip6 saddr ${addresses.lan.ipv6.subnet} drop log prefix "NFT_DROP_SPOOF_ULA: " group 2
+            ip6 saddr ${addresses.lan.ipv6.subnet} log prefix "NFT_DROP_SPOOF_ULA: " group 2 drop 
 
             # drop packets if the source address is unroutable ("how did you get here?")
-            fib saddr . iif oif missing drop log prefix "NFT_DROP_UNROUTABLE: " group 3
+            fib saddr . iif oif missing log prefix "NFT_DROP_UNROUTABLE: " group 3 drop 
           }
           chain ingress_wan {
             type filter hook ingress device ${interfaces.wan.name} priority -500;
             ip protocol icmp limit rate 5/second accept
-            ip protocol icmp counter drop "NFT_DROP_ICMP: " group 4
+            ip protocol icmp counter log prefix "NFT_DROP_ICMP: " group 4 drop 
             # always allow router advertisement (type 134 AKA nd-router-advert)
             ip6 nexthdr icmpv6 icmpv6 type nd-router-advert accept
             ip6 nexthdr icmpv6 limit rate 5/second accept
-            ip6 nexthdr icmpv6 counter drop "NFT_DROP_ICMP6: " group 4
+            ip6 nexthdr icmpv6 counter log prefix "NFT_DROP_ICMP6: " group 4 drop 
 
             # drop incoming packets that are not locally addressed
-            fib daddr . iif type != local drop "NFT_DROP_UNROUTABLE_WAN: " group 3
+            fib daddr . iif type != local log prefix "NFT_DROP_UNROUTABLE_WAN: " group 3 drop 
           }
         }
 
         table inet global {
           chain inbound_wan {
-            ct state new log prefix "NFT_INBOUND_NEW_CONN: " group 5 rate limt 5/second
+            ct state new log prefix "NFT_INBOUND_NEW_CONN: " group 5 limit rate 5/second
             # accept all icmp requests -- TODO this could be constrained or even rate-limited
             ip protocol icmp accept
             ip6 nexthdr icmpv6 accept
@@ -81,7 +81,7 @@
             ip6 saddr ${addresses.lan.ipv6.subnet} oifname ${interfaces.wan.name} drop
             
             # log outbound connections
-            ip saddr 192.168.3.0/24 oifname wan0 ct state new log prefix "NFT_FORWARD_NEW_CONN: " group 6 rate limit 5/second
+            ip saddr 192.168.3.0/24 oifname wan0 ct state new log prefix "NFT_FORWARD_NEW_CONN: " group 6 limit rate 5/second
 
             ct state vmap { established : accept, related : accept, invalid : drop }
 
@@ -91,7 +91,7 @@
           chain postrouting {
             type nat hook postrouting priority 100; policy accept;
             # do nat for ipv4 clients
-            ip saddr ${addresses.lan.ipv4.subnet} oifname ${interfaces.wan.name} masquerade log prefix "NFT_NAT: " group 11
+            ip saddr ${addresses.lan.ipv4.subnet} oifname ${interfaces.wan.name} log prefix "NFT_NAT: " group 7 masquerade 
           }
         }
       '';
