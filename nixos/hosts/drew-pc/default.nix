1c1ba48037a0f2ec18d727339732dce1a53abe8f
Andrew Eggleston <egglestonandrew927@gmail.com>
2025-04-03 02:29:27 -0400

    drew-pc: update nvidia driver

--- nixos/hosts/drew-pc/default.nix
+++ nixos/hosts/drew-pc/default.nix
@@ -43,8 +43,8 @@
       open = false;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "570.86.16"; # use new 570 drivers
-        sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
+        version = "570.133.07"; # use new 570 drivers
+        sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
         openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
         settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
         usePersistenced = false;
