504852cf3fdafa62073b01410d90654eaa65bb1f
Andrew Eggleston <egglestonandrew927@gmail.com>
2025-11-30 14:41:15 -0500

    fix bitwig rendering problem

--- home-manager/andreweggleston/features/desktop/music-production.nix
+++ home-manager/andreweggleston/features/desktop/music-production.nix
@@ -1,7 +1,10 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   home.packages = [
     pkgs.bitwig-studio5
     pkgs.renoise
     pkgs.audacity
   ];
+
+  home.file.".BitwigStudio/graphics-backend".text = "skia-gl";
 }
