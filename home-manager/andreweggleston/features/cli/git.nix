3f6a5152d3f19a352180786bafb37a65f0a5bbdc
andreweggleston <egglestonandrew927@gmail.com>
2026-01-12 22:00:53 -0500

    update to 25.11
    Signed-off-by: andreweggleston <egglestonandrew927@gmail.com>

--- home-manager/andreweggleston/features/cli/git.nix
+++ home-manager/andreweggleston/features/cli/git.nix
@@ -6,18 +6,22 @@
 {
   programs.git = {
     enable = true;
-    userName = "Andrew Eggleston";
-    userEmail = "egglestonandrew927@gmail.com";
-    aliases = {
-      upstream-name = "!git remote | egrep -o '(upstream|origin)' | tail -1";
-      head-branch = "!basename $(git symbolic-ref refs/remotes/$(git upstream-name)/HEAD)";
-      cm = "!git checkout $(git head-branch)";
-      co = "checkout";
-      cob = "checkout -b";
-      repo-root = "rev-parse --show-toplevel";
-      rr = "rev-parse --show-toplevel";
+    settings = {
+      user = {
+        name = "Andrew Eggleston";
+        email = "egglestonandrew927@gmail.com";
+      };
+      alias = {
+        upstream-name = "!git remote | egrep -o '(upstream|origin)' | tail -1";
+        head-branch = "!basename $(git symbolic-ref refs/remotes/$(git upstream-name)/HEAD)";
+        cm = "!git checkout $(git head-branch)";
+        co = "checkout";
+        cob = "checkout -b";
+        repo-root = "rev-parse --show-toplevel";
+        rr = "rev-parse --show-toplevel";
+      };
+      init.defaultBranch = "main";
     };
-
     includes = [
       {
         path = "~/.config/git/config-sharefile.inc";
@@ -32,9 +36,5 @@
       "*.swp"
       ".idea/"
     ];
-
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
   };
 }
