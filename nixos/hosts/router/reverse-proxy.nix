7feef8a80f0b3ceebcdf762e10fb008c363972c4
Andrew Eggleston <egglestonandrew927@gmail.com>
2026-03-06 20:44:29 -0500

    router: update caddy
    Signed-off-by: Andrew Eggleston <egglestonandrew927@gmail.com>

--- nixos/hosts/router/reverse-proxy.nix
+++ nixos/hosts/router/reverse-proxy.nix
@@ -9,7 +9,7 @@
 let
   caddypkg = pkgs.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
-    hash = "sha256-s1N/csCCP2O/0buCf064R/WVwuR4qpF25pIJBBTbFYg=";
+    hash = "sha256-R1ZqQ8drcBQIH7cLq9kEvdg9Ze3bKkT8IAFavldVeC0=";
   };
 in
 {
