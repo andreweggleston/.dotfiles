9035b193198268f77af0b5074829da034cf91470
andreweggleston <egglestonandrew927@gmail.com>
2026-03-14 13:58:44 -0400

    neovim deps -> unstable

--- home-manager/andreweggleston/features/cli/neovim.nix
+++ home-manager/andreweggleston/features/cli/neovim.nix
@@ -6,53 +6,37 @@
 {
   programs.neovim.enable = true;
   programs.neovim.plugins = [
-    pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-    pkgs.vimPlugins.nvim-treesitter
+    pkgs.unstable.vimPlugins.nvim-treesitter.withAllGrammars
+    pkgs.unstable.vimPlugins.nvim-treesitter
   ];
   programs.neovim.extraPackages = [
-    pkgs.tree-sitter-grammars.tree-sitter-c
-    pkgs.tree-sitter-grammars.tree-sitter-cpp
-    pkgs.tree-sitter-grammars.tree-sitter-cmake
-    pkgs.clang-tools
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-c
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-cpp
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-cmake
+    pkgs.unstable.clang-tools
 
-    pkgs.tree-sitter
-    pkgs.lua54Packages.jsregexp
-    pkgs.tree-sitter-grammars.tree-sitter-lua
-    pkgs.tree-sitter-grammars.tree-sitter-nix
-    pkgs.tree-sitter-grammars.tree-sitter-go
-    pkgs.tree-sitter-grammars.tree-sitter-python
-    pkgs.tree-sitter-grammars.tree-sitter-bash
-    pkgs.tree-sitter-grammars.tree-sitter-regex
-    pkgs.tree-sitter-grammars.tree-sitter-markdown
-    pkgs.tree-sitter-grammars.tree-sitter-json
+    pkgs.unstable.tree-sitter
+    pkgs.unstable.lua54Packages.jsregexp
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-lua
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-nix
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-python
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-bash
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-regex
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-markdown
+    pkgs.unstable.tree-sitter-grammars.tree-sitter-json
 
-    pkgs.nodejs_24
-    pkgs.nodePackages_latest.vscode-json-languageserver
-    pkgs.fzf
-    pkgs.lazygit
-    pkgs.lua-language-server
-    pkgs.luajitPackages.jsregexp
-    pkgs.nixd
-    pkgs.go
-    pkgs.gopls
-    pkgs.gofumpt
-    pkgs.stylua
-    pkgs.cargo
-    pkgs.rustc
-    pkgs.basedpyright
-    pkgs.terraform-ls
-    pkgs.terraform-lsp
-    pkgs.dart
-    pkgs.ruff
-    pkgs.nixfmt-rfc-style
-    pkgs.starlark-rust
-    pkgs.zls
-    pkgs.ripgrep
-    pkgs.ueberzugpp
-    pkgs.viu
-    pkgs.chafa
-    pkgs.delve
-    pkgs.imagemagick
+    pkgs.unstable.fzf
+    pkgs.unstable.lazygit
+    pkgs.unstable.lua-language-server
+    pkgs.unstable.luajitPackages.jsregexp
+    pkgs.unstable.nixd
+    pkgs.unstable.stylua
+    pkgs.unstable.basedpyright
+    pkgs.unstable.ruff
+    pkgs.unstable.nixfmt
+    pkgs.unstable.zls
+    pkgs.unstable.ripgrep
+    pkgs.unstable.chafa
   ];
   home.file = {
     ".config/nvim" = {
