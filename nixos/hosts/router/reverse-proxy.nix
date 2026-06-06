c2ddb1c9d236583ae01cc7467e141d5f6191860c
Andrew Eggleston <egglestonandrew927@gmail.com>
2026-06-06 01:39:25 -0400

    router: caddy: bump pkgs version

--- nixos/hosts/router/reverse-proxy.nix
+++ nixos/hosts/router/reverse-proxy.nix
@@ -9,7 +9,7 @@
 let
   caddypkg = pkgs.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
-    hash = "sha256-R1ZqQ8drcBQIH7cLq9kEvdg9Ze3bKkT8IAFavldVeC0=";
+    hash = "sha256-BKUsUoBE1IjnD9Xu8kTVkbRqqk2qvNtFDD/pvVkfRmI=";
   };
 in
 {
