bb472ea02f60cb2f02a6e38c3c76a62a2d4ac884
Andrew Eggleston <egglestonandrew927@gmail.com>
2026-08-05 01:45:20 -0400

    nvim: zls
    Signed-off-by: Andrew Eggleston <egglestonandrew927@gmail.com>

--- home-manager/andreweggleston/features/cli/nvim/lua/plugins/lsp.lua
+++ home-manager/andreweggleston/features/cli/nvim/lua/plugins/lsp.lua
@@ -88,6 +88,7 @@ return { -- LSP keymaps
         ruff = {},
         clangd = {},
         nixd = {},
+        zls = {},
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
           -- Use this to add any additional keymaps
